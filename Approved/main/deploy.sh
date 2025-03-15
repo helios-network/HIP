e6246c3212b15c874c777221364b015210cac545
jguyet <jguyet.weoohh@gmail.com>
2025-03-15 14:55:02 +0100

    Deploy - Book https://helios-network.github.io/HIP

---
+++ Approved/main/deploy.sh
@@ -0,0 +1,11 @@
+sed -i -e 's/book\//book-tmp/g' ./.gitignore
+rm -f .gitignore-e
+git add .gitignore
+# delete gh-pages
+git push origin `git subtree split --prefix book gh-pages`:gh-pages --force
+# Add book Commit
+git add book && git commit -m "Deploy - Book https://helios-network.github.io/HIP"
+# Push Book to gh-pages
+git subtree push --prefix book origin gh-pages
+# Back to last commit
+git checkout HEAD~1
